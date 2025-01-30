ff7d5b6c52c1269daf2c5db35a2764afb49c6502
sofiagarcia358 <159475085+sofiagarcia358@users.noreply.github.com>
2025-01-30 13:44:17 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+let DOM = document.querySelector("#root");
+
+let contenedor = document.createElement('div');
+contenedor.className = "div-contenedor"
+
+
+let divHeader = document.createElement('div');
+divHeader.className = "div-header"
+contenedor.appendChild(divHeader);
+
+let divProgreso = document.createElement('div');
+divProgreso.className = "div-progreso"
+contenedor.appendChild(divProgreso);
+
+let divTablero = document.createElement('div');
+divTablero.className = "div-tablero"
+contenedor.appendChild(divTablero); 
+
+let divFooter = document.createElement('div');
+divFooter.className = "div-footer"
+contenedor.appendChild(divFooter);
+
+
+
+
+DOM.appendChild(contenedor);
