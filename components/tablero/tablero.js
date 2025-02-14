d8f85ac9741d5034a2096fc673609843831cc370
Sofis <sofiagarcia.2021p@gmail.com>
2025-02-14 00:05:58 -0600

    darle vuelta cartas

--- components/tablero/tablero.js
+++ components/tablero/tablero.js
@@ -3,7 +3,21 @@ import {todas_las_cartas} from"./data.js"
 function item(contenedor){
     let div =document.createElement('div');
     div.className ="carta";
-    div.textContent= contenedor;
+
+    let adl = document.createElement('div');
+    adl.className = "front";
+    adl.textContent = ""; 
+
+    let back = document.createElement('div');
+    back.className = "back";
+    back.textContent = contenedor; 
+
+    div.appendChild(adl);
+    div.appendChild(back);
+
+    div.addEventListener('click', () => {
+        div.classList.toggle('flipped');
+    });
    
     return div;
 
@@ -13,10 +27,8 @@ function cargarCartas(){
     div.className = "div-tableros1";
 
     todas_las_cartas.forEach((letra) => {
-        div.appendChild(item(letra));
-        
-    });
-
+            div.appendChild(item(letra));
+        });
 
     return div;
 }
